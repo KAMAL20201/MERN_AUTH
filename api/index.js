e1e3bcbc2195beda532d72d9aabf1aabdf89e824
KAMAL20201 <arorakamal706@gmail.com>
2024-04-28 23:39:12 +0530

    Initial SignUp added

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+
+mongoose
+  .connect(process.env.mongo)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+
+app.listen(3001, () => console.log("Server started on port 3001"));
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
