78dfcda0c3b73fe4811020add69fcb459353622d
KAMAL20201 <arorakamal706@gmail.com>
2024-05-09 00:20:08 +0530

    Fix: Frontend separated

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import User from "./models/user.model.js";
-import path from "path";
 dotenv.config();
 
 mongoose
@@ -17,13 +16,8 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
 const app = express();
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 app.use(express.json());
 app.use(cookieParser());
 
